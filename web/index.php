9b1623494b50c4fcb9f6fe79a352ca5845a623f6
3rr0r007 <33449411+3rr0r007@users.noreply.github.com>
2017-11-07 11:37:29 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,10 +6,10 @@
   <body>
     <h2>Buongiorno 5IA</h2>
     <form id="email-form" method="POST" action="send.php"> <!--send.php code: https://gist.github.com/ganmahmud/fabe0821a230409f39b2#file-send-php-->
-      <p><input type="text" id="name" name="name" placeholder="Inserisci nome" /></p>
-      <p><input type="email" id="email" name="email" placeholder="Inserisci email" /></p>
-      <p><textarea name="msg" id="msg" cols="22" placeholder="Scrivi il tuo messaggio qui"></textarea></p>
-      <button type="submit">Send</button>
+      <input type="text" id="name" name="name" placeholder="Inserisci nome" />
+      <input type="email" id="email" name="email" placeholder="Inserisci email" />
+      <textarea name="msg" id="msg" cols="22" placeholder="Scrivi il tuo messaggio qui"></textarea>
+      <button type="submit">Invia</button>
     </form>
   </body>
 </html>
