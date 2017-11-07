449800a8e46368f9de5dea137927e3e5b4be7b4a
3rr0r007 <33449411+3rr0r007@users.noreply.github.com>
2017-11-07 11:34:15 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
 <html>
-<body>
-
-<?php
-echo "Buongiorno 5IA!";
-?>
-
-</body>
+  <head>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="web-link.js"></script>
+  </head>
+  <body>
+    <h2>php and ajax email on basic html form layout</h2>
+    <form id="email-form" method="POST" action="send.php"> <!--send.php code: https://gist.github.com/ganmahmud/fabe0821a230409f39b2#file-send-php-->
+      <input type="text" id="name" name="name" placeholder="Inserisci nome" />
+      <input type="email" id="email" name="email" placeholder="Inserisci email" />
+      <textarea name="msg" id="msg" cols="22" placeholder="Scrivi il tuo messaggio qui"></textarea>
+      <button type="submit">Send</button>
+</form>
+  </body>
 </html>
