0be3bc5a7bda71463aa729415dda3e1b4d157f37
3rr0r007 <33449411+3rr0r007@users.noreply.github.com>
2017-11-07 11:36:01 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,12 +4,12 @@
     <script src="web-link.js"></script>
   </head>
   <body>
-    <h2>php and ajax email on basic html form layout</h2>
+    <h2>Buongiorno 5IA</h2>
     <form id="email-form" method="POST" action="send.php"> <!--send.php code: https://gist.github.com/ganmahmud/fabe0821a230409f39b2#file-send-php-->
-      <input type="text" id="name" name="name" placeholder="Inserisci nome" />
-      <input type="email" id="email" name="email" placeholder="Inserisci email" />
-      <textarea name="msg" id="msg" cols="22" placeholder="Scrivi il tuo messaggio qui"></textarea>
+      <p><input type="text" id="name" name="name" placeholder="Inserisci nome" /></p>
+      <p><input type="email" id="email" name="email" placeholder="Inserisci email" /></p>
+      <p><textarea name="msg" id="msg" cols="22" placeholder="Scrivi il tuo messaggio qui"></textarea></p>
       <button type="submit">Send</button>
-</form>
+    </form>
   </body>
 </html>
